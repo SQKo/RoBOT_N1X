dcaa1188539754ed05cfef9c7dfdd2ad4c116121
SQKo <87897282+SQKo@users.noreply.github.com>
2022-04-16 22:13:38 +0700

    disable error reporting

--- public/interaction.php
+++ public/interaction.php
@@ -6,6 +6,8 @@ use Discord\Interaction;
 use Discord\InteractionResponseType;
 use Discord\InteractionType;
 
+error_reporting(0);
+
 $CLIENT_PUBLIC_KEY = getenv('CLIENT_PUBLIC_KEY');
 
 $signature = $_SERVER['HTTP_X_SIGNATURE_ED25519'];
